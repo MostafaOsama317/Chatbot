92a4f66c824c4fa1e6e2a95fb8501ff8cb56dba6
mostafa <mostafasleem305@gmail.co>
2025-12-25 13:04:02 +0200

    structure is created

---
+++ template.sh
@@ -0,0 +1,16 @@
+mkdir -p src
+mkdir -p research
+
+#create files 
+touch src/__init__.py
+touch src/healper.py
+touch src/prompt.py
+
+touch .env
+touch setup.py
+touch app.py
+touch requirements.txt
+
+touch research/trails.ipynb
+
+echo "Project structure created successfully."
